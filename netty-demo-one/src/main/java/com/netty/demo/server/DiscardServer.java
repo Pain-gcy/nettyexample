f3b058cbca25cc1c436b22a796712a9098e69f42
guochunyuan@gitv.cn <guochunyuan@gitv.cn>
2018-09-17 16:42:25 +0800

    update

--- netty-demo-one/src/main/java/com/netty/demo/server/DiscardServer.java
+++ netty-demo-one/src/main/java/com/netty/demo/server/DiscardServer.java
@@ -14,8 +14,17 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
  */
 public class DiscardServer {
     public void run(int port) throws Exception {
-//        两个线程组
-        //用于处理服务器端接收客户端连接
+        /**
+         * NioEventLoopGroup 是用来处理I/O操作的多线程事件循环器，
+         * Netty提供了许多不同的EventLoopGroup的实现用来处理不同传输协议。
+         * 在这个例子中我们实现了一个服务端的应用，
+         * 因此会有2个NioEventLoopGroup会被使用。
+         * 第一个经常被叫做‘boss’，用来接收进来的连接。
+         * 第二个经常被叫做‘worker’，用来处理已经被接收的连接，
+         * 一旦‘boss’接收到连接，就会把连接信息注册到‘worker’上。
+         * 如何知道多少个线程已经被使用，如何映射到已经创建的Channels上都需要依赖于EventLoopGroup的实现，
+         * 并且可以通过构造函数来配置他们的关系。
+         */
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         //进行网络通信（读写）
         EventLoopGroup workerGroup = new NioEventLoopGroup();
